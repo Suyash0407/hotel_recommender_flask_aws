a48bbd5f77a474ab7d4e39b9c74fdeec9397a0d8
Suyash Mudholkar <55226930+Suyash0407@users.noreply.github.com>
2020-05-19 22:10:32 +0530

    code

--- hotel.py
+++ hotel.py
@@ -12,7 +12,7 @@ import nltk
 from nltk.stem import PorterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
 import re
-
+nltk.download('punkt')
 # =============================================================================
 
 # =============================================================================
